63927e979f0c6be6f9e7739c8d3aeb9bcfdd18f5
Aman Kumar <amankr1619@gmail.com>
2021-04-13 18:18:53 +0530

    Added env

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 25,
     auth: {
-        user: process.env.user,
-        pass: process.env.pass
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
 })
 //[1]
