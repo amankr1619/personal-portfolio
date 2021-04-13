b873f8dbb8629f98913147573ae8d8ca6531a721
Aman Kumar <amankr1619@gmail.com>
2021-04-13 20:02:36 +0530

    Changed port

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,7 +4,7 @@ const emailPass = process.env.pass
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 25,
+    port: 465,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
